874e2c20283e46d88159a063f032e552dfe575db
Wajih-Khalil <134858920+Wajih-Khalil@users.noreply.github.com>
2023-11-24 04:16:03 +0500

    Update assign.js

--- assign.js
+++ assign.js
@@ -1,105 +1,97 @@
-/* _____________________________________________________________________
-//                            CHAPTER 49 -------52
-// ________________________________________________________________________*/
 
+// CHAPTER 49-52
 
+// Answer 1 (data collection)
 
-// _______________________QUESTION NO 01 (Collect data)_________//
+// Question 1
+function submitForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
 
-
-// // ---FOR HTML---//
-
-
-// <input id="email" type="text" placeholder="Enter your Email">
-// <input id="passWord" type="text" placeholder="Enter your pasword">
-// <button onclick="chec()">click</button>
-
-// // --------------------------------------------------------//
-
-
-function chec() {
-    var a = document.getElementById("email")
-
-    var b = document.getElementById("passWord")
-    document.write(a.value + "</br>")
-    document.write(b.value)
+    const userDataDiv = document.getElementById('userData');
+    userDataDiv.innerHTML = `<p>Name: ${name}</p><p>Email: ${email}</p>`;
 }
 
-// _______________________QUESTION NO 02 ()_________//
-
-
-// _______________________QUESTION NO 03 (TODO APP)_________//
-
-// // ---FOR HTML---//
-
-
-// <div>
-// <input id="todo" type="text" placeholder="enter todo">
-// <button onclick="myFunction()">Add task</button>
-// <button onclick="deleteAll()">Delete All</button>
-
-// </div>
-
-// <div>
-// <ol id="list">
-
-// </ol>
-
-// </div>
-
-// // --------------------------------------------------------//
-
-var list = document.getElementById("list")
-
-function myFunction() {
-    var item = document.getElementById("todo")
-
-    // create li tag with java text node
-    var li = document.createElement("li")
-    var listtext = document.createTextNode(item.value)
-    li.appendChild(listtext)
-    list.appendChild(li)
-
-    // create delete button
-    var delBtn = document.createElement("button")
-    var delText = document.createTextNode("delete")
-    delBtn.appendChild(delText)
-    li.appendChild(delBtn)
-    delBtn.setAttribute("class", "text")
-    delBtn.setAttribute("onclick", "deleteItem(this)")
-
-
-    // create Edit Buttons
-    var editBtn = document.createElement("button")
-    var editText = document.createTextNode("Edit")
-    editBtn.appendChild(editText)
-    li.appendChild(editBtn)
-    editBtn.setAttribute("class", "text")
-    editBtn.setAttribute("onclick", "editItem(this)")
-
-
-
-    item.value = ""
-    console.log(li)
+// Question 2
+function showDetails() {
+    const fullDetails = document.getElementById('fullDetails');
+    fullDetails.classList.toggle('hidden');
 }
 
-// for delete item
+// Question 3
+function addStudent() {
+    const name = document.getElementById('studentName').value;
+    const grade = document.getElementById('studentGrade').value;
 
-function deleteItem(e) {
-    e.parentNode.remove()
+    const tableBody = document.querySelector('#studentTable tbody');
+    const newRow = tableBody.insertRow();
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+
+    cell1.textContent = name;
+    cell2.textContent = grade;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function () {
+        tableBody.removeChild(newRow);
+    };
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.onclick = function () {
+        // Show a hidden form for editing
+        document.getElementById('editForm').classList.remove('hidden');
+
+        // Populate the form with current values
+        document.getElementById('editName').value = name;
+        document.getElementById('editGrade').value = grade;
+
+        // Set up an event listener for the edit form
+        document.getElementById('editForm').onsubmit = function (event) {
+            event.preventDefault();
+            // Update the table cell values
+            cell1.textContent = document.getElementById('editName').value;
+            cell2.textContent = document.getElementById('editGrade').value;
+            // Hide the edit form
+            document.getElementById('editForm').classList.add('hidden');
+        };
+    };
+
+    cell3.appendChild(deleteButton);
+    cell3.appendChild(editButton);
 }
 
-// for delete All
+// Question 3 (Continued)
+function updateStudent() {
+    // Update the table cell values (similar to the edit functionality)
+    const tableBody = document.querySelector('#studentTable tbody');
+    const editName = document.getElementById('editName').value;
+    const editGrade = document.getElementById('editGrade').value;
 
+    const editedRow = document.querySelector('#studentTable tbody tr.editing');
+    editedRow.cells[0].textContent = editName;
+    editedRow.cells[1].textContent = editGrade;
 
-function deleteAll() {
-    list.innerHTML = ""
+    // Hide the edit form
+    document.getElementById('editForm').classList.add('hidden');
 }
 
-// for Edit
+document.addEventListener('DOMContentLoaded', function () {
+    // Add a listener to the table body to handle the edit functionality
+    const tableBody = document.querySelector('#studentTable tbody');
+    tableBody.addEventListener('click', function (event) {
+        const target = event.target;
+        if (target.tagName.toLowerCase() === 'button' && target.textContent.toLowerCase() === 'edit') {
+            const selectedRow = target.parentNode.parentNode;
+            // Mark the selected row for editing
+            selectedRow.classList.add('editing');
+        }
+    });
+});
 
-function editItem(ee) {
-    var editvalue = prompt("enter edit value", ee.parentNode.firstChild.nodeValue)
 
     ee.parentNode.firstChild.nodeValue = editvalue
 }
